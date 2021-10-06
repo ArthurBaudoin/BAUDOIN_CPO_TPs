e5353d91b19b2bcd3c28bd26629856a920ba5b5d
arthu <arthu@192.168.114.132>
2021-10-06 12:18:29 +0200

    FIN EXO1

--- TP1_MANIP_NB/src/tp1_manip_nb/TP1_MANIP_NB.java
+++ TP1_MANIP_NB/src/tp1_manip_nb/TP1_MANIP_NB.java
@@ -4,6 +4,8 @@
  */
 package tp1_manip_nb;
 
+import java.util.Scanner;
+
 /**
  *
  * @author arthu
@@ -14,7 +16,24 @@ public class TP1_MANIP_NB {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("saisir deux entiers");
+        double nb1 = sc.nextDouble();
+        double nb2 = sc.nextDouble();
+        
+        double somme1 = nb1 + nb2;
+        double diff1 = nb1 - nb2;
+        double produit1 = nb1 * nb2;
+        System.out.println(" La somme est " + somme1);
+        System.out.println(" La différence est " + diff1);
+        System.out.println(" Le produit est " + produit1);
+        
+        double q1 = nb1/nb2;
+        double r = nb1%nb2;
+        System.out.println(" Le quotient est " + q1);
+        System.out.println(" Le reste est " + r);
+        
+        
+
     }
-    
 }
