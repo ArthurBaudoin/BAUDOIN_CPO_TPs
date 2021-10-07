39b88c8bf92e8bb756e6d0ccd9e9a309763d0326
arthu <arthu@192.168.114.132>
2021-10-07 23:01:38 +0200

    EXO4 FIN

--- TP_exo4/src/tp_exo4/TP_exo4.java
+++ TP_exo4/src/tp_exo4/TP_exo4.java
@@ -4,6 +4,9 @@
  */
 package tp_exo4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author arthu
@@ -14,7 +17,31 @@ public class TP_exo4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         int [] TabEntier = new int[6];
+        for (int i=0; i <=5; i++){
+            TabEntier[i]=0;
+        }
+        int m;
+
+        
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("saisir le nombre :");
+        m=sc.nextInt(); 
+        
+        
+        
+        Random generateurAleat = new Random();
+        for (int i=0; i<m; i++){
+            int n = generateurAleat.nextInt(6);
+            TabEntier[n] = TabEntier[n]+1;
+        }
+  
+        
+        
+        for (int i=0; i<=5; i++){
+            System.out.println("face "+(i+1)+"du dé : "+((TabEntier[i])/(float)(m))*100+"%");
+    }
     }
     
 }
