f7dd02f082e4731af3661c661ffdbfa4cda57f18
arthu <arthu@192.168.114.132>
2021-10-08 17:23:29 +0200

    EXO 3

--- TP1_exo3/src/tp1_exo3/TP1_exo3.java
+++ TP1_exo3/src/tp1_exo3/TP1_exo3.java
@@ -4,6 +4,9 @@
  */
 package tp1_exo3;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author arthu
@@ -13,5 +16,64 @@ public class TP1_exo3 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {   
-    } }
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        Random generateurAleat = new Random();
+
+        System.out.println("Choisir la difficulté");
+        System.out.println("1) Facile");
+        System.out.println("2) Normal");
+        System.out.println("3) Difficile");
+        int diff = sc.nextInt();
+        while (diff > 3 || diff < 1) {
+            System.out.println("ERREUR Selectioner une valeur entre 1 et 3");
+            System.out.println("Choisir la difficulté");
+            diff = sc.nextInt();
+        }
+        int x = 100;
+        switch (diff) {
+            case 1 -> {
+                System.out.println("Cherche entre 1 et 100");
+            }
+            case 2 -> {
+                x = 1000;
+                System.out.println("Cherche entre 1 et 1000");
+            }
+            case 3 -> {
+                x = 2000;
+                System.out.println("Cherche entre 1 et 2000");
+
+            }
+        }
+
+        int alea = generateurAleat.nextInt(x);
+
+        System.out.println("Entre 0 et " + x + " : ");
+        int nb = sc.nextInt();
+
+        int z = 1;
+        do {
+            if (nb < alea) {
+                System.out.println("trop petit");
+                nb = sc.nextInt();
+            }
+            if (nb > alea) {
+                System.out.println("trop grand");
+                nb = sc.nextInt();
+            }
+            z++;
+        } while (nb != alea);
+        if (nb == alea) {
+            System.out.println(
+                    "exact en " + z + " coups");
+        }
+    }
+   
+    
+    
+    
+    
+    
+    
+    } 
