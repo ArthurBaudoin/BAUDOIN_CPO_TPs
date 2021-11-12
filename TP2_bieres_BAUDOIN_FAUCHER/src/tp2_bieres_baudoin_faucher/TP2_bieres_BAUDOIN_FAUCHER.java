ce6de81fc277668660d2981349a2421b48db629c
arthu <arthu@192.168.114.132>
2021-11-12 17:17:59 +0100

    TP 2 EXO1 DEBUT

--- TP2_bieres_BAUDOIN_FAUCHER/src/tp2_bieres_baudoin_faucher/TP2_bieres_BAUDOIN_FAUCHER.java
+++ TP2_bieres_BAUDOIN_FAUCHER/src/tp2_bieres_baudoin_faucher/TP2_bieres_BAUDOIN_FAUCHER.java
@@ -15,6 +15,42 @@ public class TP2_bieres_BAUDOIN_FAUCHER {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+  /*
+        BouteilleBiere uneBiere = new BouteilleBiere() ;
+        
+        uneBiere.Nom = "Cuvée des trolls";
+        uneBiere.degreAlcool =  7.0 ;
+        uneBiere.brasserie = "Dubuisson";
+        uneBiere.ouverte = false;
+        
+        uneBiere.lireEtiquette();
+
+        BouteilleBiere uneBiere2 = new BouteilleBiere() ;
+        
+        uneBiere2.Nom = "Leffe";
+        uneBiere2.degreAlcool = 6.6 ;
+        uneBiere2.brasserie = "Abbaye de Leffe";
+        uneBiere2.ouverte = false; 
+        
+        uneBiere2.lireEtiquette(); 
+        */
+
+        BouteilleBiere Leffe = new BouteilleBiere("Leffe", 6.6,"Abbaye de Leffe") ;
+        BouteilleBiere Trolls = new BouteilleBiere("Cuvée des trolls",7.0 ,"Dubuisson") ;
+        BouteilleBiere Pelforth = new BouteilleBiere("Pelforth",5.8 ,"brasserie du Pelican") ;
+        BouteilleBiere Chouffe = new BouteilleBiere("Chouffe", 8.0 ,"brasserie d'Achouffe") ;
+        BouteilleBiere demon = new BouteilleBiere("Bière du démon", 12.0 ,"brasserie de Gayant") ;
+        
+        
+        Leffe.lireEtiquette();
+        Trolls.lireEtiquette();
+        Pelforth.lireEtiquette();
+        Chouffe.lireEtiquette();
+        demon.lireEtiquette();
+    
+    
+    
     }
+    
     
 }
