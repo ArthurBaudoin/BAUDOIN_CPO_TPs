42731ab5c5ebf2e242ba3fc7183b8b789f69acc0
arthu <arthu@192.168.114.132>
2021-11-22 15:34:22 +0100

    debut grille

--- SP4_console_BAUDOIN _FAUCHER/src/sp4_console_baudoin/_faucher/Grille.java
+++ SP4_console_BAUDOIN _FAUCHER/src/sp4_console_baudoin/_faucher/Grille.java
@@ -9,5 +9,14 @@ package sp4_console_baudoin._faucher;
  * @author arthu
  */
 public class Grille {
+    Cellule CellulesJeu [][] = new Cellule[6][7];
+    
+    Grille(){
+        for (int i=0; i<6; i++){
+            for (int j=0; j<7; j++){
+                CellulesJeu [i][j] = new Cellule();
+            }
+        }
+    }
     
 }
